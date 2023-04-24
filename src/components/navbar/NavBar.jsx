8eca3bdca5513485f81c05d9b37295d8b5cd3fc5
etchepared <88214684+etchepared@users.noreply.github.com>
2023-04-24 01:19:39 -0300

    navbar functionalities in progress

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,8 +1,21 @@
 import React from "react";
 import "./navbar.css";
 import { Person, Mail, LinkedIn } from "@material-ui/icons";
+import Swal from "sweetalert2";
+import { FileCopy } from "@material-ui/icons";
+import { CopyToClipboard } from "react-copy-to-clipboard";
 
 export default function NavBar({ menuOpen, setMenuOpen }) {
+  const handleClick = () => {
+    Swal.fire({
+      position: "top-end",
+      icon: "success",
+      title: "Email copiado",
+      showConfirmButton: false,
+      timer: 1500,
+    });
+  };
+
   return (
     <div className="navbarTop">
       <div className={"navbar " + (menuOpen && "active")}>
@@ -16,10 +29,23 @@ export default function NavBar({ menuOpen, setMenuOpen }) {
               <span>+54 9 2964 551421</span>
             </div>
             <div className="itemContainer">
-              <Mail className="icon" /> <span>etcheparede@gmail.com</span>
+              <div className="copyMail">
+                <CopyToClipboard text="etcheparede@gmail.com">
+                  <button onClick={handleClick}>
+                    <Mail className="icon" /> <span>etcheparede@gmail.com</span>
+                    <FileCopy className="icon" />
+                  </button>
+                </CopyToClipboard>
+              </div>
             </div>
             <div className="itemContainer">
-              <LinkedIn className="icon" /> <span>/etcheparede</span>
+              <a
+                href="https://www.linkedin.com/in/etcheparede/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <LinkedIn className="icon" /> <span>/etcheparede</span>
+              </a>
             </div>
           </div>
           <div className="right">
