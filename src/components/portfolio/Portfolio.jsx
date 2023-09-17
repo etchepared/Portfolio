87783fde93aa656863fdbf9944cd7c7517ae510e
etchepared <etcheparede@gmail.com>
2023-09-17 19:22:37 -0300

    fix: Fix links to repos and deploy

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -10,16 +10,67 @@ export default function Portfolio() {
       </div>
       <div className="container">
         <div className="item">
+          <div className="project"></div>
           <img src="assets/Lok.png" alt="https://pffrontend-fafd3.web.app/" />
           <h3>Lok Ecommerce</h3>
+          <div className="links">
+            <div className="repo-link">
+              <a
+                href="https://github.com/maxisosa89/PF_backend_ecommerce"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Repo back
+              </a>
+            </div>
+            <div className="repo-link">
+              <a
+                href="https://github.com/nicolasmayorquinduran/frontend_PF"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Repo front
+              </a>
+            </div>
+          </div>
         </div>
 
         <div className="item">
+          <div className="project"></div>
           <img
             src="assets/pokepedia.png"
-            alt="https://pokemonclient.herokuapp.com/"
+            alt="https://pokepedia-vite.netlify.app/"
           />
           <h3>PokePedia</h3>
+          <div className="links">
+            <div className="repo-link">
+              <a
+                href="https://github.com/etchepared/apipokepedia"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Repo back
+              </a>
+            </div>
+            <div className="repo-link">
+              <a
+                href="https://github.com/etchepared/PI-client-vite"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Repo front
+              </a>
+            </div>
+            <div className="deploy-link">
+              <a
+                href="https://pokepedia-vite.netlify.app/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Deploy
+              </a>
+            </div>
+          </div>
         </div>
       </div>
       <a className="down" href="#contact">
